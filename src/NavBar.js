79e0065161f9219f1cef8beaf3321b27e99e7492
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-07-09 14:11:39 -0500

    feat: Navbar

---
+++ src/NavBar.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from "@reach/router";
+import { css } from "@emotion/core";
+import colors from "./colors";
+
+const NavBar = () => {
+  const [padding, setPadding] = useState(15);
+
+  return (
+    <header
+      onClick={() => setPadding(padding + 15)}
+      css={css`
+        background-color: ${colors.secondary};
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        `}
+        role="img"
+        aria-label="logo"
+      >
+        Puddle
+      </span>
+    </header>
+  );
+};
+
+export default NavBar;
